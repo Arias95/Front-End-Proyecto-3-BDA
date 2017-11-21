91b4eb562f6554d984b9fa955992f5386aed7892
Andres Arias <andres.arias12@gmail.com>
2017-11-20 23:11:18 -0600

    Registro y login listos

--- src/app/client/client.js
+++ src/app/client/client.js
@@ -3,6 +3,10 @@ angular.module('bdatienda')
         $routeProvider.when('/', {
             templateUrl: '/src/app/client/login.html',
             controller: 'ClientController'
+        })
+        .when('/register', {
+            templateUrl: '/src/app/client/register.html',
+            controller: 'ClientController'
         });
     }])
 
@@ -11,5 +15,13 @@ angular.module('bdatienda')
             $scope.login = function () {
                 userService.usuario = $scope.username;
                 $location.path('/home');
+            }
+
+            $scope.reg = function() {
+                $location.path('/register');
+            }
+
+            $scope.cancel = function() {
+                $location.path('/');
             }
         }]);
